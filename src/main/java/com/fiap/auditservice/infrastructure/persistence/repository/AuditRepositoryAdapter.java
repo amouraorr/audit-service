a511922ed17513bff8b4a0cfb05216f08ac232d0
Alex Mourão <amouraorr@yahoo.com.br>
2025-11-06 19:03:22 -0300

    implementações e ajustes

--- src/main/java/com/fiap/auditservice/infrastructure/persistence/repository/AuditRepositoryAdapter.java
+++ src/main/java/com/fiap/auditservice/infrastructure/persistence/repository/AuditRepositoryAdapter.java
@@ -1,4 +1,3 @@
-/*
 package com.fiap.auditservice.infrastructure.persistence.repository;
 
 import com.fiap.auditservice.domain.AuditRecord;
@@ -6,44 +5,43 @@ import com.fiap.auditservice.domain.port.out.AuditRepositoryPort;
 import com.fiap.auditservice.infrastructure.persistence.jpa.AuditJpaEntity;
 import com.fiap.auditservice.infrastructure.persistence.jpa.AuditJpaRepository;
 import com.fiap.auditservice.infrastructure.persistence.mapper.AuditEntityMapper;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class AuditRepositoryAdapter implements AuditRepositoryPort {
 
-    private final AuditJpaRepository jpaRepository;
+    private final AuditJpaRepository repository;
     private final AuditEntityMapper mapper;
 
-    public AuditRepositoryAdapter(AuditJpaRepository jpaRepository, AuditEntityMapper mapper) {
-        this.jpaRepository = jpaRepository;
+    public AuditRepositoryAdapter(AuditJpaRepository repository, AuditEntityMapper mapper) {
+        this.repository = repository;
         this.mapper = mapper;
     }
 
     @Override
-    public AuditRecord save(AuditRecord auditRecord) {
-        AuditJpaEntity entity = mapper.toEntity(auditRecord);
-        AuditJpaEntity saved = jpaRepository.save(entity);
+    public AuditRecord save(AuditRecord record) {
+        AuditJpaEntity entity = mapper.toEntity(record);
+        AuditJpaEntity saved = repository.save(entity);
         return mapper.toDomain(saved);
     }
 
     @Override
     public Optional<AuditRecord> findById(UUID id) {
-        return jpaRepository.findById(id).map(mapper::toDomain);
+        return repository.findById(id).map(mapper::toDomain);
     }
 
     @Override
     public List<AuditRecord> findAll(int limit) {
         int size = Math.max(1, limit);
-        var page = jpaRepository.findAll(PageRequest.of(0, size, Sort.by(Sort.Direction.DESC, "createdAt")));
+        var page = repository.findAll(PageRequest.of(0, size, Sort.by(Sort.Direction.DESC, "createdAt")));
         return page.stream()
                 .map(mapper::toDomain)
                 .collect(Collectors.toList());
     }
-}*/
+}
